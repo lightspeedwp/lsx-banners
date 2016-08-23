725e330c94ed498d556462b725313b4752dba33d
Fernando Tessmann <nandotess85@gmail.com>
2016-08-23 09:38:31 -0300

    conditional test to avoid JS error in pages with empty image banner

--- assets/js/lsx-banner.js
+++ assets/js/lsx-banner.js
@@ -2,8 +2,12 @@ var LSX_Banners = {
 	initThis: function(){
 		if (jQuery('body').hasClass('page-has-banner')) {
 			var $bannerImage = jQuery('#lsx-banner .page-banner.rotating .page-banner-image'),
+				bannerImageObj;
+			
+			if ($bannerImage.length > 0) {
 				bannerImageObj = $bannerImage.attr('data-banners').split(',');
-			$bannerImage.css('background-image','url(' + bannerImageObj[Math.floor(Math.random() * bannerImageObj.length)] + ')');
+				$bannerImage.css('background-image','url(' + bannerImageObj[Math.floor(Math.random() * bannerImageObj.length)] + ')');
+			}
 		}
 	}
 }
