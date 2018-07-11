0cef1df24ee46fc9babfa4642ea3d55a26217302
Warwick Booth <warwick@lsdev.biz>
2018-07-11 13:42:25 +0200

    Added in a conditional tag for the banners_inner_bottom hook.

--- classes/class-lsx-banners-frontend.php
+++ classes/class-lsx-banners-frontend.php
@@ -411,7 +411,7 @@ class LSX_Banners_Frontend extends LSX_Banners {
 									<?php do_action( 'lsx_banner_container_bottom' ); ?>
 								</div>
 
-								<?php //lsx_banner_inner_bottom(); ?>
+								<?php if ( function_exists( 'lsx_banner_inner_bottom' ) ) { lsx_banner_inner_bottom(); }; ?>
 							</div>
 						</div>
 						<?php
