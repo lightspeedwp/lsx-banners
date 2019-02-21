ec9567e2503c72e09d29fce72b21ec8c4749e2d4
Warwick Booth <warwick@lsdev.biz>
2019-02-21 17:10:28 +0200

    Making the ui display tab dynamic

--- includes/settings/display.php
+++ includes/settings/display.php
@@ -50,10 +50,7 @@ asdfasdfasdfasdf
 			<li><a href="#ui-search" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Search', 'lsx-banners' ); ?></a></li>
 		<?php } ?>
 
-		<?php if ( class_exists( 'lsx\member_directory\classes\Core' ) ) { ?>
-			<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'LSX_Team' ) || class_exists( 'LSX_Testimonials' ) || class_exists( 'LSX_Projects' ) || class_exists( 'LSX_Services' ) || class_exists( 'LSX_Blog_Customizer' ) || class_exists( 'LSX_Sharing' ) || class_exists( 'LSX_Videos' ) || class_exists( 'LSX_Videos' ) || class_exists( 'LSX_Search' ) ) ? '' : 'active'; ?>
-			<li><a href="#ui-member-directory" class="<?php echo esc_attr( $class_active ); ?>"><?php esc_html_e( 'Member Directory', 'lsx-banners' ); ?></a></li>
-		<?php } ?>
+		<?php do_action( 'lsx_framework_display_tab_headings_bottom', 'display' ); ?>
 	</ul>
 
 	<?php if ( class_exists( 'LSX_Banners' ) ) { ?>
@@ -160,17 +157,6 @@ asdfasdfasdfasdf
 			<table class="form-table">
 				<tbody>
 					<?php do_action( 'lsx_framework_display_tab_content', 'search' ); ?>
-				</tbody>
-			</table>
-		</div>
-	<?php } ?>
-
-	<?php if ( class_exists( 'lsx\member_directory\classes\Core' ) ) { ?>
-		<?php $class_active = ( class_exists( 'LSX_Banners' ) || class_exists( 'LSX_Currencies' ) || class_exists( 'LSX_Team' ) || class_exists( 'LSX_Testimonials' ) || class_exists( 'LSX_Projects' ) || class_exists( 'LSX_Services' ) || class_exists( 'LSX_Blog_Customizer' ) || class_exists( 'LSX_Sharing' ) || class_exists( 'LSX_Videos' ) || class_exists( 'LSX_Search' ) ) ? '' : 'active'; ?>
-		<div id="ui-member-directory" class="ui-tab <?php echo esc_attr( $class_active ); ?>">
-			<table class="form-table">
-				<tbody>
-				<?php do_action( 'lsx_framework_display_tab_content', 'member-directory' ); ?>
 				</tbody>
 			</table>
 		</div>
