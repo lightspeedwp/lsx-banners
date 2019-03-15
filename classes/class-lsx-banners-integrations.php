76c99136b727dc13950b77163149c99f68171f07
Warwick Booth <warwick@lsdev.biz>
2019-03-15 21:41:06 +0800

    * Dev - Added in a theme option to allow enabling banners on the events page

--- classes/class-lsx-banners-integrations.php
+++ classes/class-lsx-banners-integrations.php
@@ -11,6 +11,13 @@
 class LSX_Banners_Integrations {
 
 	/**
+	 * Holds the plugins integration post types
+	 *
+	 * @var      array|Lsx_Banners
+	 */
+	public $post_types = array( 'tribe_events' );
+
+	/**
 	 * Initialize the plugin by setting localization, filters, and administration functions.
 	 *
 	 * @since 1.0.0
@@ -19,6 +26,16 @@ class LSX_Banners_Integrations {
 	 */
 	public function __construct( ) {
 		add_filter( 'lsx_banner_allowed_post_types', array( $this, 'lsx_banner_allowed_post_types' ), 10, 1 );
+
+		if ( false !== $this->post_types ) {
+			foreach ( $this->post_types as $post_type ) {
+				if ( function_exists( 'tour_operator' ) ) {
+					add_action( 'lsx_to_framework_' . $post_type . '_tab_archive_settings_top', array( $this, 'enable_banners_setting' ), 20 );
+				} else {
+					add_action( 'lsx_framework_' . $post_type . '_tab_content_top', array( $this, 'enable_banners_setting' ), 20 );
+				}
+			}
+		}
 	}
 
 	/**
@@ -29,5 +46,29 @@ class LSX_Banners_Integrations {
 			$post_types[] = 'tribe_events';
 		}
 		return $post_types;
+	}
+
+	public function enable_banners_setting( $tab = 'general' ) {
+		if ( in_array( $tab, $this->post_types ) ) {
+			?>
+			<tr class="form-field">
+				<th scope="row">
+					<label for="banners_enabled"><?php esc_html_e( 'Enable Banners', 'lsx-banners' ); ?></label>
+				</th>
+				<td>
+					<input type="checkbox" {{#if banners_enabled}} checked="checked" {{/if}} name="banners_enabled" />
+				</td>
+			</tr>
+			<?php
+		}
+	}
+
+	public function enable_banners( $enable, $post_type ) {
+		print_r('hello');
+
+		if ( in_array( $post_type, $this->post_types ) ) {
+
+		}
+		return $enable;
 	}
 }
